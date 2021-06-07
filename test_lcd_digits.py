ff0efb438ff2f45b05953b07a6bb3286c66f2317
panggggg <p.picczy@gmail.com>
2021-06-07 23:25:51 +0700

    write a failing test. (add a test function push 3 should be return LCDDigits 3.)

--- test_lcd_digits.py
+++ test_lcd_digits.py
@@ -38,3 +38,15 @@ class TestLCDDigits(unittest.TestCase):
         actual = lcd_digits.create_LCD(number=2)
         # assert
         self.assertEquals(expected, actual)
+
+    def test_create_LDCDigits_push_3_should_be_return_LCDDigits_3(self):
+        # arrange
+        first_line = "._.\n"
+        second_line = "._|\n"
+        third_line = "._|"
+        expected = first_line + second_line + third_line
+        # action
+        lcd_digits = LCDDigits()
+        actual = lcd_digits.create_LCD(number=3)
+        # assert
+        self.assertEquals(expected, actual)
