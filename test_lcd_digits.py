2df4212b4a6942c99b210d99bda395685695072e
panggggg <p.picczy@gmail.com>
2021-06-10 08:57:09 +0700

    add a new test function

--- test_lcd_digits.py
+++ test_lcd_digits.py
@@ -82,3 +82,36 @@ class TestLCDDigits(unittest.TestCase):
         actual = self.lcd_digits.create_LCD(number=12)
         # assert
         self.assertEquals(expected, actual)
+
+    def test_create_LDCDigits_push_100_should_be_return_LCDDigits_100(self):
+        # arrange
+        first_line = "...._.._.\n"
+        second_line = "..||.||.|\n"
+        third_line = "..||_||_|"
+        expected = first_line + second_line + third_line
+        # action
+        actual = self.lcd_digits.create_LCD(number=100)
+        # assert
+        self.assertEquals(expected, actual)
+
+    def test_create_LDCDigits_push_101_should_be_return_LCDDigits_102(self):
+        # arrange
+        first_line = "...._....\n"
+        second_line = "..||.|..|\n"
+        third_line = "..||_|..|"
+        expected = first_line + second_line + third_line
+        # action
+        actual = self.lcd_digits.create_LCD(number=101)
+        # assert
+        self.assertEquals(expected, actual)
+
+    def test_create_LDCDigits_push_102_should_be_return_LCDDigits_102(self):
+        # arrange
+        first_line = "...._.._.\n"
+        second_line = "..||.|._|\n"
+        third_line = "..||_||_."
+        expected = first_line + second_line + third_line
+        # action
+        actual = self.lcd_digits.create_LCD(number=102)
+        # assert
+        self.assertEquals(expected, actual)
